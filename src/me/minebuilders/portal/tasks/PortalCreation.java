62920bf73ef9b9df717895f2101052960da31fa9
Spoopy <thirdgenvalentine@gmail.com>
2016-09-19 20:26:57 -1000

    Portal Direction Changes
    Fixed Bug where portals being created would face the wrong direction.

--- src/me/minebuilders/portal/tasks/PortalCreation.java
+++ src/me/minebuilders/portal/tasks/PortalCreation.java
@@ -1,7 +1,12 @@
 package me.minebuilders.portal.tasks;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import me.minebuilders.portal.Bound;
 import me.minebuilders.portal.IP;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -14,13 +19,52 @@ public class PortalCreation implements Runnable, Listener {
 
 	private int timerID;
 	private Bound b;
-
+	
+	@SuppressWarnings("deprecation")
 	public PortalCreation(Bound b) {
 		this.b = b;
 		Bukkit.getPluginManager().registerEvents(this, IP.instance);
 		timerID = Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(IP.instance, this, 100L); //100 ticks is more then enough
+		
+		for (Location l : b.getBlocks(Material.PORTAL)) {
+			l.getBlock().setType(Material.AIR);
+		}
+		
+		List<Integer> xValues = new ArrayList<Integer>();
+		
 		for (Location l : b.getBlocks(Material.AIR)) {
-			l.getBlock().setType(Material.PORTAL);
+			Double dX = l.getX();
+			xValues.add(dX.intValue());
+		}
+		
+		boolean movesZ = false;
+		int amount = xValues.size();
+		int correctCount = 0;
+		for (int xx : xValues) {
+			if (xx == xValues.get(0)) {
+				correctCount++;
+			}
+		}
+		
+		if (correctCount == amount) {
+			movesZ = true;
+		}
+		
+		if (movesZ) {
+			//MOVES ON Z
+			for(Location l : b.getBlocks(Material.AIR)) {
+				l.getBlock().setType(Material.PORTAL);
+				l.getBlock().setData((byte)2);
+			}
+			Bukkit.broadcastMessage("Portal moves on Z!");
+	
+		} else {
+			//MOVES ON X
+			for(Location l : b.getBlocks(Material.AIR)) {
+				l.getBlock().setType(Material.PORTAL);
+			}
+			Bukkit.broadcastMessage("Portal moves on X!");
+			
 		}
 	}
 
