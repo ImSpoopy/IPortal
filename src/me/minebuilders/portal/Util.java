941cf6f2f4d7a3868eeb152c0d9c4bef2ae32e40
Spoopy <thirdgenvalentine@gmail.com>
2016-09-19 19:55:42 -1000

    Utils Color Changed
    Purple is in.

--- src/me/minebuilders/portal/Util.java
+++ src/me/minebuilders/portal/Util.java
@@ -10,9 +10,9 @@ public class Util {
 
 	private static final Logger log = Logger.getLogger("Minecraft");
 
-	public static void log(String s) { log.info("[IPortal] " + s); }
+	public static void log(String s) { log.info("[iPortal] " + s); }
 	
-	public static void warning(String s) { log.warning("[IPortal] " + s); }
+	public static void warning(String s) { log.warning("[iPortal] " + s); }
 
 	public static boolean hp(CommandSender sender, String s) {
 		if (sender.hasPermission("iportal." + s)) {
@@ -22,7 +22,7 @@ public class Util {
 	}
 
 	public static void msg(CommandSender sender, String s) {
-		sender.sendMessage(ChatColor.DARK_RED + "[" + ChatColor.DARK_AQUA + "IPortal" +ChatColor.DARK_RED +"] " + ChatColor.AQUA + ChatColor.translateAlternateColorCodes('&', s)); 
+		sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.DARK_PURPLE + "iPortal" +ChatColor.DARK_GRAY +"] " + ChatColor.GRAY + ChatColor.translateAlternateColorCodes('&', s)); 
 	}
 	
 	public static void scm(CommandSender sender, String s) {
@@ -30,7 +30,7 @@ public class Util {
 	}
 
 	public static void broadcast(String s) { 
-		Bukkit.getServer().broadcastMessage(ChatColor.DARK_RED + "[" + ChatColor.DARK_AQUA + "IPortal" +ChatColor.DARK_RED +"] " + ChatColor.AQUA + ChatColor.translateAlternateColorCodes('&', s)); 
+		Bukkit.getServer().broadcastMessage(ChatColor.DARK_GRAY + "[" + ChatColor.DARK_PURPLE + "iPortal" +ChatColor.DARK_GRAY +"] " + ChatColor.GRAY + ChatColor.translateAlternateColorCodes('&', s)); 
 	}
 
 	public static boolean isInt(String str) {
